9fe8f2869d5cee6119143bfab6aae109849fbfdb
santicano <canosantiago404@gmail.com>
2023-06-02 11:21:22 -0300

    Añadido funcion print para visualizar el arbol

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -28,5 +28,21 @@ class BinaryTree {
         return root;
     }
 
-    
+
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+let tree = new BinaryTree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+prettyPrint(tree.root);
