454bffec26d240cc5e7ea575eda7a00adb22bce8
santicano <canosantiago404@gmail.com>
2023-06-02 11:01:56 -0300

    añadida funcion buildTree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,4 +10,23 @@ class BinaryTree {
     constructor(array) {
         this.root = this.buildTree(array);
     }
+
+    sortAndRemoveDuplicates(array) {
+        const sorted = [...new Set(array)].sort((a, b) => a - b);
+        return sorted;
+    }
+
+    buildTree(array) {
+        let sorted = this.sortAndRemoveDuplicates(array);
+        if (sorted.length === 0) return null;
+        const mid = parseInt(sorted.length / 2);
+        const root = new Node(
+            sorted[mid],
+            this.buildTree(sorted.slice(0, mid)),
+            this.buildTree(sorted.slice(mid + 1))
+        );
+        return root;
+    }
+
+    
 }
